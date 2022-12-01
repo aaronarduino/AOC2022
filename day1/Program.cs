e92151c3cc556b77319d53149e0e6ec485ee7e1b
Aaron Alexander <aalexander@transitionsgroup.net>
2022-12-01 17:52:41 -0600

    feat: added day1

---
+++ day1/Program.cs
@@ -0,0 +1,42 @@
+﻿using day1.Services;
+
+var reader = new FileReader("", "input.txt", "test.txt");
+List<string>? input = reader.ReadLines();
+
+if (input != null)
+{
+    input.Add("");
+    int current = 0;
+    int third = 0;
+    int second = 0;
+    int first = 0;
+
+    foreach (var line in input)
+    {
+        if (line.Length > 0)
+        {
+            current += Convert.ToInt32(line);
+        }
+        else
+        {
+            if (current > first)
+            {
+                third = second;
+                second = first;
+                first = current;
+            }
+            else if (current > second && current != first)
+            {
+                third = second;
+                second = current;
+            }
+            else if (current > third && current != second)
+            {
+                third = current;
+            }
+            current = 0;
+        }
+    }
+
+    Console.WriteLine(first + second + third);
+}
---
+++ day1/Services/FileReader.cs
@@ -0,0 +1,65 @@
+﻿namespace day1.Services
+{
+    public class FileReader
+    {
+        private string BasePath { get; set; }
+        private string InputFileName { get; set; }
+        private string TestFileName { get; set; }
+
+        public FileReader(string basePath, string inputFileName, string testFileName)
+        {
+            BasePath = basePath;
+            InputFileName = inputFileName;
+            TestFileName = testFileName;
+        }
+
+        public string? ReadAll(bool test = false)
+        {
+            string? text = "";
+
+            try
+            {
+                if (test)
+                {
+                    text = File.ReadAllText(BasePath + TestFileName);
+                }
+                else
+                {
+                    text = File.ReadAllText(BasePath + InputFileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                text = null;
+            }
+
+            return text;
+        }
+
+        public List<string>? ReadLines(bool test = false)
+        {
+            List<string>? text = null;
+
+            try
+            {
+                if (test)
+                {
+                    text = File.ReadLines(BasePath + TestFileName).ToList();
+                }
+                else
+                {
+                    text = File.ReadLines(BasePath + InputFileName).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                text = null;
+            }
+
+            return text;
+        }
+    }
+}
+
