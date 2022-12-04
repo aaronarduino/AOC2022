4874956888844327eac7572abc926c0531d543d0
Aaron Alexander <aalexander@transitionsgroup.net>
2022-12-04 17:12:05 -0600

    days 2 and 3

---
+++ day2/Program.cs
@@ -0,0 +1,79 @@
+﻿using AOCHelper;
+
+var reader = new FileReader("", "input.txt", "test.txt");
+List<string>? input = reader.ReadLines();
+
+Dictionary<string, string> Lose = new Dictionary<string, string>()
+{
+    ["A"] = "C",
+    ["C"] = "B",
+    ["B"] = "A",
+};
+
+Dictionary<string, string> Win = new Dictionary<string, string>()
+{
+    ["C"] = "A",
+    ["B"] = "C",
+    ["A"] = "B",
+};
+
+Dictionary<string, string> Strats = new Dictionary<string, string>()
+{
+    ["X"] = "Loss",
+    ["Y"] = "Draw",
+    ["Z"] = "Win",
+};
+
+Dictionary<string, int> ShapeValues = new Dictionary<string, int>()
+{
+    ["A"] = 1, // Rock
+    ["B"] = 2, // Paper
+    ["C"] = 3, // Scissors
+};
+
+if (input != null)
+{
+    Console.WriteLine($"Rounds found: {input.Count}");
+    int roundScore = 0;
+    int gameScore = 0;
+
+    foreach (var round in input)
+    {
+        // 1. Parse shapes for round
+        string[] shapes = round.Split(" ");
+        if (shapes.Length != 2)
+        {
+            Console.WriteLine($"Error: unable to parse round ({round})");
+            System.Environment.Exit(1);
+        }
+
+        // 2. Get shapes for each side
+        string opponentShape = shapes[0];
+        string myStrat = shapes[1];
+
+        // 3. Calc round score
+        if (myStrat == "X") // Lose
+        {
+            var myShape = Lose[opponentShape];
+            roundScore = ShapeValues[myShape];
+        }
+        else if (myStrat == "Y") // Draw
+        {
+            var myShape = opponentShape;
+            roundScore = ShapeValues[myShape] + 3;
+        }
+        else if (myStrat == "Z") // Win
+        {
+            var myShape = Win[opponentShape];
+            roundScore = ShapeValues[myShape] + 6;
+        }
+
+        // 4. Add round score to game score
+        gameScore += roundScore;
+
+        // Reset round score
+        roundScore = 0;
+    }
+
+    Console.WriteLine(gameScore);
+}
---
+++ day3/Program.cs
@@ -0,0 +1,66 @@
+﻿using AOCHelper;
+
+var reader = new FileReader("", "input.txt", "test.txt");
+List<string>? input = reader.ReadLines();
+
+List<char> priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToList();
+
+if (input != null)
+{
+    // Part 1
+    int total = 0;
+
+    foreach (var line in input)
+    {
+        var chunked = line.Chunk(line.Length / 2).ToList();
+
+        string firstComp = new string(chunked[0]);
+        string secondComp = new string(chunked[1]);
+
+        var diff = firstComp.Intersect(secondComp).ToList();
+        char commonChar = diff[0];
+
+        int commonCharPriority = priorities.IndexOf(commonChar) + 1;
+
+        total += commonCharPriority;
+    }
+
+    Console.WriteLine(total);
+
+    total = 0;
+
+    // Part 2
+    int count = 1;
+
+    string first = "";
+    string second = "";
+    string third = "";
+
+    foreach (var line in input)
+    {
+        if (count == 3)
+        {
+            third = line;
+
+            string firstTwo = new string(first.Intersect(second).ToArray());
+            var diff = firstTwo.Intersect(third).ToList();
+            char commonChar = diff[0];
+
+            int commonCharPriority = priorities.IndexOf(commonChar) + 1;
+
+            total += commonCharPriority;
+            count = 0;
+        }
+        else if (count == 2)
+        {
+            second = line;
+        }
+        else if (count == 1)
+        {
+            first = line;
+        }
+        count++;
+    }
+
+    Console.WriteLine(total);
+}
